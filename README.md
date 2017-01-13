7b26ec3e98cd0d1e2f0c4ec5b48dc8d78e746120
Scott Fox <scottfox5@gmail.com>
2017-01-13 10:45:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Scott Fox
+##Color Picker Game
+
+When the page is loaded, the user is alerted to select a specific block based on its color. The correct block is randomly generated.
+
+When the user selects the correct box, they are alerted of their success, the block turns pink, and the game starts over.
+If the user selects the wrong box, they are alerted that they are incorrect and told to guess again.
---
+++ app.js
@@ -0,0 +1,66 @@
+// var colorArray = ['red', 'green', 'yellow', 'blue', 'purple'];
+// // var correctGuess = false;
+//
+// $(function(){
+//
+//   addBlock();
+//   pickRandomBlock();
+//
+// });
+//
+// function addBlock(){
+//   for (var i = 0; i < colorArray.length; i++) {//Adds different color blocks - number of blocks equal to colors.
+//     var $colorBlock = ('<div class="colorBlock"></div>');
+//     var $color = $($colorBlock).css('background-color', colorArray[i]);
+//     $($color).attr('id', colorArray[i]).appendTo('#blockSection');//Appends block to DOM, and gives it id equal to its color.
+//   };
+// };
+//
+// function pickRandomBlock(){
+//   var randomColor = colorArray[randomNumber(0, colorArray.length - 1)];//Sets a variable to one of the random colors.
+//   alert('Pick the ' + randomColor + ' block.');//Alerts the user to pick a box
+//
+//   $('#'+randomColor+'').click(function(){//When correct box is clicked, user is alerted of their success.
+//     setTimeout(function(){$('div').css('background-color', 'pink')}, 2000);
+//     alert("Correct!");
+//   });
+//
+//   $(".colorBlock:not(#"+randomColor+")").click(function(){//When the wrong box is clicked, user is alerted of failure.
+//     alert("Wrong! Guess again");
+//   });
+// };
+//
+// function randomNumber(min, max){
+//   return Math.floor(Math.random() * (1 + max - min) + min);
+// };
+
+
+
+$(function(){
+
+  var colorArray = ['red', 'green', 'yellow', 'blue'];
+
+  for (var i = 0; i < colorArray.length; i++) {//Adds different color blocks - number of blocks equal to colors.
+    var $colorBlock = ('<div class="colorBlock"></div>');
+    var $color = $($colorBlock).css('background-color', colorArray[i]);
+    $($color).attr('id', colorArray[i]).appendTo('#blockSection');//Appends block to DOM, and gives it id equal to its color.
+  };
+
+  var randomColor = colorArray[randomNumber(0, colorArray.length - 1)];//Sets a variable to one of the random colors.
+  alert('Pick the ' + randomColor + ' block.');//Alerts the user to pick a box
+
+  $('#'+randomColor+'').click(function(){//When correct box is clicked, user is alerted of their success.
+      $(this).css('background-color', 'pink');
+      setTimeout(function(){location.reload()}, 2000);
+    alert("Correct!");
+  });
+
+  $(".colorBlock:not(#"+randomColor+")").click(function(){//When the wrong box is clicked, user is alerted of failure.
+    alert("Wrong! Guess again");
+  });
+
+});
+
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (1 + max - min) + min);
+};
